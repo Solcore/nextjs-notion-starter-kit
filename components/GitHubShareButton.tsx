31859c3a4ceadef2c8c8da7a4ae386ca14f4b369
Solcore <129042618+Solcore@users.noreply.github.com>
2023-03-31 17:52:14 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -23,7 +23,7 @@ export const GitHubShareButton: React.FC = () => {
           top: 0,
           right: 0,
           border: 0,
-          display: none,
+          display:none,
           transform: 'scale(1, 1)'
         }}
         aria-hidden='true'
