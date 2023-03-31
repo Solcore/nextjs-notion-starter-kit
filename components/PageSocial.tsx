3777d299c42a9c8c060b583248c1ca1540a86f75
Solcore <129042618+Solcore@users.noreply.github.com>
2023-03-31 22:26:24 +0800

    Update PageSocial.tsx

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -16,7 +16,7 @@ interface SocialLink {
 const socialLinks: SocialLink[] = [
   config.twitter && {
     name: 'twitter',
-    href: `https://twitter.com/${config.twitter}`,
+    href: `https://solcore1999.notion.site/Resume-76ccff1e3cb6431bae06d65c7c5ce651`,
     title: `Twitter @${config.twitter}`,
     icon: (
       <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
